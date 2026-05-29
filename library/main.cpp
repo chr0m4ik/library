fadba58c102c77da730b6dc9822e2e1ccb91f622
chr0m4ik <148350202+chr0m4ik@users.noreply.github.com>
2026-05-29 11:08:43 +0500

    Обновлен main

--- library/main.cpp
+++ library/main.cpp
@@ -1,6 +1,11 @@
 #include "NoteLib.hpp"
+#include "tests.hpp"
+#include <Windows.h>
 
 int main() {
+    SetConsoleCP(65001);
+    SetConsoleOutputCP(65001);
     runNotebookApp();
+    //startTests();
     return 0;
 }
